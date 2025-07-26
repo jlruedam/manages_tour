b53509b62f4b17a4cb69a7add8705149364156af
jlruedam <jlruedam@gmail.com>
2025-07-26 08:26:07 -0500

    feat: Implement sale calculation functionality with dynamic total update

---
+++ static/js/newSale.js
@@ -0,0 +1,15 @@
+const sale_value_tour = document.getElementById("sale_value_tour");
+const sale_quantity = document.getElementById("sale_quantity");
+const sale_total = document.getElementById("sale_total");
+
+
+const calculate_total_sale = () => {
+    console.log('Total Sale');
+    const value = parseFloat(sale_value_tour.value) || 0;
+    const quantity = parseInt(sale_quantity.value) || 0;
+    sale_total.value = value * quantity;
+}
+
+sale_value_tour.addEventListener('change', calculate_total_sale)
+sale_quantity.addEventListener('change', calculate_total_sale)
+sale_total.addEventListener('click', calculate_total_sale);
