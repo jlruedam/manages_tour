bba94f61b9e4088071b7fa85b0f61179c0b91a75
jlruedam <jlruedam@gmail.com>
2025-07-26 08:35:45 -0500

    fix: Update event listeners for sale total calculation to use 'input' event for better responsiveness

--- static/js/newSale.js
+++ static/js/newSale.js
@@ -2,7 +2,6 @@ const sale_value_tour = document.getElementById("sale_value_tour");
 const sale_quantity = document.getElementById("sale_quantity");
 const sale_total = document.getElementById("sale_total");
 
-
 const calculate_total_sale = () => {
     console.log('Total Sale');
     const value = parseFloat(sale_value_tour.value) || 0;
@@ -10,6 +9,6 @@ const calculate_total_sale = () => {
     sale_total.value = value * quantity;
 }
 
-sale_value_tour.addEventListener('change', calculate_total_sale)
-sale_quantity.addEventListener('change', calculate_total_sale)
+sale_value_tour.addEventListener('input', calculate_total_sale);
+sale_quantity.addEventListener('input', calculate_total_sale);
 sale_total.addEventListener('click', calculate_total_sale);
