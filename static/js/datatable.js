277c510041a0ef060146d8182b03879945bb30c3
jlruedam <jlruedam@gmail.com>
2025-07-28 08:08:48 -0500

    responsive table list-sales

--- static/js/datatable.js
+++ static/js/datatable.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-    $('#tabla-ventas-general').DataTable();
-  });
+    $('#tabla-ventas-general').DataTable({
+        responsive: true,
+        // columnDefs: [
+        //     { responsivePriority: 1, targets: 0 },  // Nombre = siempre visible
+        //     { responsivePriority: 1, targets: 1 },  // Correo = siguiente más importante
+        //     { responsivePriority: 1, targets: 2 },  // Nombre = siempre visible
+        //     { responsivePriority: 4, targets: 3 }, 
+        //     { responsivePriority: 10000, targets: 4 } // Edad = menos importante
+        // ],
+        language: {
+        url: "//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json"
+        }
+    });
+});
